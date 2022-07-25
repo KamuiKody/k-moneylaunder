fba5223fbdc2449e3c38234e22a8d15b018f5445
KamuiKody <62488881+KamuiKody@users.noreply.github.com>
2022-07-25 12:22:32 -0400

    Lazy Developement.
    missed a comma

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config = {
-    UseTarget = true
+    UseTarget = true,
     location = vector4(195.17, -933.77, 30.7, 144.5),
     item = false, -- set to item name if you want items instead of cash
     value = 1 -- how many items/dollars u get per $ of marked money
